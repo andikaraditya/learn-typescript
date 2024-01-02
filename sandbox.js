15008193a66ee3d76b2817b871fc9b7661c1088a
Andika <andikaraditya1@gmail.com>
2024-01-02 12:21:30 +0700

    lesson 5

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,24 @@
 console.log("Typescript");
+function circ(diameter) {
+    return diameter * Math.PI;
+}
+console.log(circ(20));
+var names = [];
+names.push("John");
+names.push("Jack");
+console.log(names);
+var ages = [];
+ages.push(20);
+ages.push(25);
+console.log(ages);
+var mixed = [];
+mixed.push("John");
+mixed.push(20);
+console.log(mixed);
+var ninja;
+ninja = {
+    name: "jack",
+    age: 20,
+    beltColor: "black"
+};
+console.log(ninja);
--- sandbox.ts
+++ sandbox.ts
@@ -1,1 +1,39 @@
 console.log("Typescript")
+
+function circ(diameter:number) {
+    return diameter * Math.PI
+}
+
+console.log(circ(20))
+
+const names: string[] = []
+names.push("John")
+names.push("Jack")
+
+console.log(names)
+
+const ages: number[] = []   
+ages.push(20)
+ages.push(25)
+
+console.log(ages)
+
+const mixed: (string|number)[] = []
+mixed.push("John")
+mixed.push(20)
+
+console.log(mixed)
+
+let ninja: {
+    name: string,
+    age: number,
+    beltColor: string
+}
+
+ninja = {
+    name: "jack",
+    age: 20,
+    beltColor: "black"
+}
+
+console.log(ninja)
